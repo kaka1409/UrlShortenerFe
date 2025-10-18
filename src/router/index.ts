85fb9141fc41862d71e9a1090d7ef4ff0ca21879
kaka1409 <khanhphqgcs230461@fpt.edu.vn>
2025-10-18 09:13:16 +0700

    Add: not found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/HomePage.vue'
 import Login from '@/views/LoginPage.vue'
 import Register from '@/views/RegisterPage.vue'
+import NotFound from '@/views/404NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -37,6 +38,11 @@ const router = createRouter({
       component: Register,
     },
 
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFound,
+    }
   ],
 })
 
---
+++ src/views/404NotFound.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import Header from '@/components/Header.vue';
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <Header></Header>
+  <div class="h-full w-full flex flex-col justify-center items-center gap-4">
+    <h1 class="text-6xl text-white font-bold">404, NOT FOUND</h1>
+    <p class="text-white text-xl">Page not exist, seem like you are lost</p>
+    <RouterLink to="/" class="
+      font-bold text-lg text-white px-4 py-2 rounded-lg border-2 border-white
+      hover:cursor-pointer hover:bg-white hover:text-black-violet transition-all duration-300
+    ">Back to home</RouterLink>
+  </div>
+</template>
